abaed6e49cc1abbe0a11c5328f0d8683da7d27a7
JKorf <jankorf91@gmail.com>
2024-02-06 20:58:40 +0100

    Updated ExtensionMethods namespaces

--- CoinGecko.Net/ExtensionMethods/CryptoClientExtensions.cs
+++ CoinGecko.Net/ExtensionMethods/CryptoClientExtensions.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace CryptoExchange.Net.Clients
+namespace CryptoExchange.Net.Interfaces
 {
     /// <summary>
     /// Extensions for the ICryptoRestClient and ICryptoSocketClient interfaces
