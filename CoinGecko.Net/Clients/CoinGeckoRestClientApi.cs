5b335f50dd3840cc49e5e14f546f9832adf0113b
Jkorf <jankorf91@gmail.com>
2025-07-14 13:53:46 +0200

    Updated CryptoExchange.Net to version 9.2.0

--- CoinGecko.Net/Clients/CoinGeckoRestClientApi.cs
+++ CoinGecko.Net/Clients/CoinGeckoRestClientApi.cs
@@ -638,7 +638,7 @@ namespace CoinGecko.Net.Clients
 
         protected override Error ParseErrorResponse(int httpStatusCode, KeyValuePair<string, string[]>[] responseHeaders, IMessageAccessor accessor, Exception? exception)
         {
-            if (!accessor.IsJson)
+            if (!accessor.IsValid)
                 return new ServerError(null, "Unknown request error", exception: exception);
 
             var code = accessor.GetValue<int?>(MessagePath.Get().Property("error_code"));
